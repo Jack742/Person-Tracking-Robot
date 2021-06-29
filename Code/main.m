0271119e6056502705f96742fba67f5cf8657e31
Jack742 <9195368+Jack742@users.noreply.github.com>
2021-06-29 14:57:06 +0100

    Seperating code into functions

--- Code/main.m
+++ Code/main.m
@@ -3,27 +3,21 @@
 dataFolder = 'models/mrcnn';
 model_name = 'maskrcnn_pretrained_person_car.mat';
 
+%%
 
-%% ####IMPORTANT##### 
-%  ##################
-%      UNCOMMENT THE BELOW CODE FOR JUST THE FIRST TIME YOU RUN THIS.
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%####IMPORTANT#####%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         ##################                              %
+%      UNCOMMENT THE BELOW CODE FOR JUST THE FIRST TIME YOU RUN THIS.     %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %trainedMaskRCNN_url = 'https://www.mathworks.com/supportfiles/vision/data/maskrcnn_pretrained_person_car.mat';
 %helper.downloadTrainedMaskRCNN(trainedMaskRCNN_url,dataFolder);
 
 
 
-%%
+%% Load Image and Resize it
 
-%Define classes. Car is needed as it is pretrained
-classNames = {'person', 'car','background'};
-numClasses = length(classNames)-1;
-
-%Load pretrained network
-net, mask_subnet = load_network(dataFolder, model_name);
-
-%Image to do prediction on
 img = imread('imgs/chefs.jpg');
 
 %resize image to required dimensions
@@ -32,18 +26,41 @@ target_size = [700 700 3];
 %Resize image to desired dimensions
 img = resized_input_img(img, target_size);
 
-%Create config
-imageSizeTrain = [800 800 3];
-params = createMaskRCNNConfig(imageSizeTrain, numClasses, classNames);
+%% %Load pretrained network and Generate Config
+net, mask_subnet = load_network(dataFolder, model_name);
+
+%% Get network Prediction
 
 %Make prediction
 [boxes,scores,labels,masks] = detectMaskRCNN(net,maskSubnet,img,params);
 
 %Visualise the Predictions
-overlayedImage = render_mask(img, masks)
+overlayedImage = render_mask(img, boxes, labels,masks);
 
+
+%% Functions
+function cam = get_camera()
+    %Serial port configuration
+    %This makes sure that the computer USB port speaks the same convention as
+    %the port on the Arduino
+    serialportlist
+    s = serialport("COM3",115200);
+    s.Terminator;
+    configureTerminator(s,"LF");
+
+    %Camera preparation
+    cameras = webcamlist;
+    cam = webcam;
+end
 
 function [net, mask_subnet] = load_network(folder, model)
+    %Input Dimensions
+    desired_image_size= [800 800 3];
+    %Define classes. Car is needed as it is pretrained
+    classNames = {'person', 'car','background'};
+    numClasses = length(classNames)-1;
+    %Create Config
+    params = createMaskRCNNConfig(desired_image_size, numClasses, classNames);
     pretrained = load(fullfile(folder, model));
     net = pretrained.net;
     maskSubnet = helper.extractMaskNetwork(net);
@@ -57,7 +74,7 @@ function img = resized_input_img(img, target_size)
     end
 end
 
-function overlayedImage = render_mask(img, masks)
+function overlayedImage = render_mask(img, boxes, labels, masks)
     if(isempty(masks))
         overlayedImage = img;
     else
